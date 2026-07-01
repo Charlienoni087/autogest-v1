b247d5983b1818c3597deeae440ff0839a15554c
unknown <Carlos@DESKTOP007>
2026-06-30 22:31:44 -0600

    Primer lanzamiento

--- Config/conexion.php
+++ Config/conexion.php
@@ -1,19 +1,15 @@
 <?php
-
 $host = "localhost";
+$db = "autogest";
 $user = "root";
-$password = "";
-$db = "farmacia";
-
-$conexion = mysqli_connect(
-    $host,
-    $user,
-    $password,
-    $db
-);
-
-if(!$conexion){
-    die("Error de conexión");
+$pass = "";
+try {
+    $conexion = new mysqli($host, $user, $pass, $db);
+    if ($conexion->connect_error) {
+        throw new Exception("Error de conexión: " . $conexion->connect_error);
+    }
+    $conexion->set_charset("utf8mb4");
+} catch (Exception $e) {
+    die($e->getMessage());
 }
-
 ?>
